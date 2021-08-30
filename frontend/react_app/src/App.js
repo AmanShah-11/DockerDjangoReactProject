2993ba80b5fe3e44cc310113fedb1fe8ece53a83
Aman Shah <a232shah@uwaterloo.ca>
2021-08-30 19:25:54 -0400

    Added redis reducers and actions

---
+++ frontend/react_app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "./components/Layout";
+import Urls from "./Urls";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <Urls />
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
